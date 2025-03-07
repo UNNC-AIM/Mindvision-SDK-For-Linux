7ef6128f8efa1b01efaf1c7ecfc8fe4240267fbf
Robert He <hnrobert@qq.com>
2025-03-08 00:26:41 +0800

    fix: simplify install script, add rules file

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Your current directory is $CURDIR. This is where the MVSDK software will b
 A=`whoami`
 B=`arch`
 
-if [ $A != 'root' ]; then
+if [ "$A" != 'root' ]; then
    echo "You have to be root to run this script"
    echo "Fail !!!"
    exit 1;
@@ -18,19 +18,8 @@ cp 99-mvusb.rules /etc/udev/rules.d/
 cp include/* /usr/include/
 echo "Copy header files"
 
-if [ $B == 'x86_64' ]; then
-	cp lib/x64/libMVSDK.so /lib
-	echo "Copy x64/libMVSDK.so to /lib"
-elif [ $B == 'aarch64' ]; then
-	cp lib/arm64/libMVSDK.so /lib
-	echo "Copy arm64/libMVSDK.so to /lib"
-elif [[ ${B:2} == '86' ]]; then
-	cp lib/x86/libMVSDK.so /lib
-	echo "Copy x86/libMVSDK.so to /lib"
-else
-	cp lib/arm/libMVSDK.so /lib
-	echo "Copy arm/libMVSDK.so to /lib"
-fi
+cp lib/x64/libMVSDK.so /lib
+echo "Copy x64/libMVSDK.so to /lib"
 
 echo "Successful"
 echo "Please  restart system  now!!!"
